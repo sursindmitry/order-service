a2202cb8a60475a8aecfc9e8422e2bb9361bfa8a
sursindmitry <dmitrijsursin@gmail.com>
2023-12-20 22:49:18 +0700

    task-2: Add checkstyle https://plugins.jetbrains.com/plugin/1065-checkstyle-idea

--- src/main/java/com/grodastr/orderservice/OrderServiceApplication.java
+++ src/main/java/com/grodastr/orderservice/OrderServiceApplication.java
@@ -3,11 +3,14 @@ package com.grodastr.orderservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Application entry point.
+ */
+
 @SpringBootApplication
 public class OrderServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OrderServiceApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(OrderServiceApplication.class, args);
+  }
 }
