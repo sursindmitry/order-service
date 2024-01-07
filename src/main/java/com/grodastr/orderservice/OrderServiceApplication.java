0c72cb5b6536683ff20d282abe0e0f3ad233b3a1
sursindmitry <dmitrijsursin@gmail.com>
2024-01-07 23:09:33 +0700

    #5: test

--- src/main/java/com/grodastr/orderservice/OrderServiceApplication.java
+++ src/main/java/com/grodastr/orderservice/OrderServiceApplication.java
@@ -11,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OrderServiceApplication {
 
   public static void main(String[] args) {
+
     SpringApplication.run(OrderServiceApplication.class, args);
   }
 }
